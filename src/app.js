7150fe74d1707a3f88d6fddda19e53fe85c194b8
Morgana Spake <40220209+MSpake@users.noreply.github.com>
2019-07-23 22:40:29 +0000

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './app.css';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="App-header">
+        <p>
+          Hello World
+        </p>
+        <p>I'm alive</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
