3a71599035e6c23ce920afc0d34c8b8ca44dee39
Morgana Spake <morgana.spake@gmail.com>
2019-07-24 16:47:07 -0700

    projects and content

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 export default class Contact extends React.Component {
   render() {
     return (
-      <section>Contact</section>
+      <section>
+        <h2>Contact</h2>
+        <ul>
+          <li>
+            <a href="mailto:morgana.spake@gmail.com"><FontAwesomeIcon icon={ faEnvelope } /></a> 
+            <a href="mailto:morgana.spake@gmail.com">morgana.spake@gmail.com</a>
+          </li>
+          <li>
+            <a href="https://github.com/MSpake"><FontAwesomeIcon icon={ faGithub } /></a> 
+            <a href="https://github.com/MSpake">github.com/MSpake</a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/morganaspake/"> <FontAwesomeIcon icon={ faLinkedin } /></a> 
+            <a href="https://www.linkedin.com/in/morganaspake/">linkedin.com/in/morganaspake</a>
+          </li>
+        </ul>
+      </section>
     );
   }
 }
--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,9 +1,58 @@
 import React from 'react';
 
+const seaWatch = {
+  title: 'Sea Watch', 
+  github: 'https://github.com/sea-watchers/sea-watch-app', 
+  image: '', 
+  technologies: 'HTML, CSS, JavaScript  |  EJS  |  Express.js  |  Postgres',
+  contributions: 'Retrieving data from multiple APIs, selectively accessing the relevant details, rendering the results to the site. Paired programming Navigator.',
+  description: 'Synthesizing data from multiple APIs, Sea Watch offers up-to-date weather conditions to the marine community. Search a coastal location and view the weather and tide data for the week.', 
+};
+const nim = {
+  title: 'Nim', 
+  github: 'https://github.com/401-advanced-javascript-nimble', 
+  image: '', 
+  technologies: 'JavaScript  |  CLI: Commander  |  Game Server/Client: Socket.io  |  MongoDB',
+  contributions: 'Socket.io game server, including 100% jest test coverage.',
+  description: 'Turn-based strategy game. Two-player integration via downloadable CLI/game client (download from npm).'
+};
+const awsomScheduler = {
+  title: 'Awsom Scheduler', 
+  github: 'https://github.com/cf-awsom-scheduler', 
+  image: '', 
+  technologies: 'JavaScript, JSX, CSS  |  C# .NET Core  |  Next Framework  |  SQL Server',
+  contributions: '.NET Core backend API, complete with SQL database interaction and roles-based authorization using JWT. Learned the basics of C# and .NET Core APIs in 1½ - 2 weeks. Team Lead.',
+  description: 'Teacher Portal and scheduling aid for the Awsom Music school. Integrates with their existing system to help reduce the load on their scheduler. Uses their provided Awsom email addresses to restrict sign up access.'
+};
+
 export default class Projects extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      projects: [
+        seaWatch,
+        nim,
+        awsomScheduler,
+      ],
+    }
+  }
   render() {
     return (
-      <section>Projects</section>
+      <article>
+        <h2>Projects</h2>
+        { this.state.projects.map( project => (
+          <section key={ project.title }>
+            <h3>{ project.title }</h3>
+            <a href={ project.github }>view code on GitHub</a>
+            <p>{ project.description }</p>
+            <ul>
+              <li>Technologies Used: { project.technologies }</li>
+              <li>Personal Contributions: { project.contributions }</li>
+            </ul>
+            <img src={ project.image }></img>
+          </section>
+        ))}
+      </article>
     );
   }
 }
