6ee7ccbc1cecf1312b1df67608b3e77d6b1fdfc9
Morgana Spake <morgana.spake@gmail.com>
2019-07-24 17:32:05 -0700

    initial landing page

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -2,17 +2,20 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import './landingPage.css';
 
 export default class LandingPage extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Morgana Spake</h1>
-        <h2>Software Developer</h2>
-        <button>View portfolio</button>
-        <FontAwesomeIcon icon={ faGithub } />
-        <FontAwesomeIcon icon={ faLinkedin } />
-        <FontAwesomeIcon icon={ faEnvelope } />
+      <div id="landingPage">
+        <section id="mainInfo">
+          <h1>Morgana Spake</h1>
+          <h2>Software Developer</h2>
+          <button>View portfolio</button>
+          <FontAwesomeIcon icon={ faGithub } />
+          <FontAwesomeIcon icon={ faLinkedin } />
+          <FontAwesomeIcon icon={ faEnvelope } />
+        </section>
       </div>
     );
   }
